e5fe72184907939ce33764bb9fe5b7f7db9d6107
magali17 <magali@uw.edu>
2019-10-29 15:00:04 -0700

    ..

--- A2.0.1_Var&Fns.R
+++ A2.0.1_Var&Fns.R
@@ -16,11 +16,11 @@ start.date = "2019-02-22"
 
 #temporal variables
 ##seasons
-winter1 <- as.Date("2018-12-21")
-winter2 <- as.Date("2019-12-21")
-spring <- as.Date("2019-03-20")
-summer <- as.Date("2019-06-21")
-fall <- as.Date("2019-09-23")
+# winter1 <- as.Date("2018-12-21")
+# winter2 <- as.Date("2019-12-21")
+# spring <- as.Date("2019-03-20")
+# summer <- as.Date("2019-06-21")
+# fall <- as.Date("2019-09-23")
 
 ##time of day
 early_am <- seq(5,8)
@@ -37,55 +37,116 @@ night <- c(seq(21,23), seq(0,4)) #? 0-4 in "night"?
 #################################### correlation plot ####################################
 #fn takes mm dataset (in long format) and creates a scatterplot comparing readings from 2 collocated instruments 
 
-colo.plot <- function(mydata=mm, primary.instrument, secondary.instrument, int.digits = 0, r2.digits = 2, rmse.digits = 0) {
+colo.plot <- function(mydata=mm, 
+                      primary.instrument, secondary.instrument, 
+                      int.digits = 0, 
+                      r2.digits = 2, 
+                      rmse.digits = 0) {
+  
   # primary.instrument = "PMPT_93"
   # secondary.instrument = "PMPT_4"
   
   data.wide <- mydata %>% 
     #select only values from desired instruments
-    filter(instrument_id == primary.instrument | 
-             instrument_id == secondary.instrument) %>%
-    #make wide format
-    spread(instrument_id, value) %>% 
-    #only look at rows where have observations for both instruments
-    drop_na(primary.instrument, secondary.instrument) 
+    filter(instrument_id %in% c(primary.instrument, secondary.instrument)) %>%
+             #make wide format
+             spread(instrument_id, value) %>% 
+             #only look at rows where have observations for both instruments
+             drop_na(primary.instrument, secondary.instrument) 
+           
+           data.wide <- data.wide %>%
+             #as.data.frame() %>%
+             #calculate percent difference in estimates
+             mutate(pct.diff = (data.wide[[secondary.instrument]] - data.wide[[primary.instrument]])/data.wide[[primary.instrument]]*100)
+           
+           
+           lm1 <- lm(formula(paste(secondary.instrument, "~", primary.instrument)), 
+                     data = data.wide)
+           
+           #percent difference between instruments
+           pct.diff.median <- round(median(data.wide$pct.diff, na.rm = T), 1)
+           
+           #rmse
+           rmse <- (data.wide[[secondary.instrument]] - data.wide[[primary.instrument]])^2 %>%
+             mean() %>%
+             sqrt() %>%
+             round(digits = rmse.digits)
+           
+           #compare primary & secondary instrument agreement 
+           data.wide %>%
+             ggplot(aes(x= data.wide[[primary.instrument]], y= data.wide[[secondary.instrument]])) + 
+             geom_point(alpha=0.3, aes(colour = route)) + 
+             geom_abline(intercept = 0, slope = 1) +
+             geom_smooth(method = "lm", aes(fill="lm")) + 
+             labs(fill="", 
+                  title = paste0(data.wide$variable[1]),
+                  subtitle = paste0("y = ", round(coef(lm1)[1], int.digits), " + ", round(coef(lm1)[2], 2), 
+                                    "x \nR2 = ", round(summary(lm1)$r.squared, r2.digits), 
+                                    "\nRMSE = ", rmse,
+                                    #"\nmedian percent diff = ", pct.diff.median, "%",
+                                    "\nno. pairs = ", nrow(data.wide)
+                  ),
+                  x=primary.instrument,
+                  y=secondary.instrument
+             )
   
-  data.wide <- data.wide %>%
-    #as.data.frame() %>%
-    #calculate percent difference in estimates
-    mutate(pct.diff = (data.wide[[secondary.instrument]] - data.wide[[primary.instrument]])/data.wide[[primary.instrument]]*100)
   
-  
-  lm1 <- lm(formula(paste(secondary.instrument, "~", primary.instrument)), 
-            data = data.wide)
-  
-  #percent difference between instruments
-  pct.diff.median <- round(median(data.wide$pct.diff, na.rm = T), 1)
-  
-  #rmse
-  rmse <- (data.wide[[secondary.instrument]] - data.wide[[primary.instrument]])^2 %>%
-    mean() %>%
-    sqrt() %>%
-    round(digits = rmse.digits)
-  
-  #compare primary & secondary instrument agreement 
-  data.wide %>%
-    ggplot(aes(x= data.wide[[primary.instrument]], y= data.wide[[secondary.instrument]])) + 
-    geom_point(alpha=0.3, aes(colour = route)) + 
-    geom_abline(intercept = 0, slope = 1) +
-    geom_smooth(method = "lm", aes(fill="lm")) + 
-    labs(fill="", 
-         title = paste0(data.wide$variable[1]),
-         subtitle = paste0("y = ", round(coef(lm1)[1], int.digits), " + ", round(coef(lm1)[2], 2), 
-                           "x \nR2 = ", round(summary(lm1)$r.squared, r2.digits), 
-                           "\nRMSE = ", rmse,
-                           #"\nmedian percent diff = ", pct.diff.median, "%",
-                           "\nno. pairs = ", nrow(data.wide)
-         ),
-         x=primary.instrument,
-         y=secondary.instrument
-    )
 }
+
+
+# same but for data in wide format 
+colo.plot.wide.data <- function(data.wide=mm.wide, 
+                      x.variable, y.variable, 
+                      int.digits = 0, 
+                      r2.digits = 2, 
+                      rmse.digits = 0) {
+  
+  # x.variable = "PMPT_93"
+  # y.variable = "PMPT_4"
+  
+  data.wide <- data.wide %>% 
+             #only look at rows where have observations for both instruments
+             drop_na(x.variable, y.variable) 
+           
+           data.wide <- data.wide %>%
+             #as.data.frame() %>%
+             #calculate percent difference in estimates
+             mutate(pct.diff = (data.wide[[y.variable]] - data.wide[[x.variable]])/data.wide[[x.variable]]*100)
+           
+           
+           lm1 <- lm(formula(paste(y.variable, "~", x.variable)), 
+                     data = data.wide)
+           
+           #percent difference between instruments
+           pct.diff.median <- round(median(data.wide$pct.diff, na.rm = T), 1)
+           
+           #rmse
+           rmse <- (data.wide[[y.variable]] - data.wide[[x.variable]])^2 %>%
+             mean() %>%
+             sqrt() %>%
+             round(digits = rmse.digits)
+           
+           #compare primary & secondary instrument agreement 
+           data.wide %>%
+             ggplot(aes(x= data.wide[[x.variable]], y= data.wide[[y.variable]])) + 
+             geom_point(alpha=0.3, aes(colour = route)) + 
+             geom_abline(intercept = 0, slope = 1) +
+             geom_smooth(method = "lm", aes(fill="lm")) + 
+             labs(fill="", 
+                  title = paste0(data.wide$variable[1]),
+                  subtitle = paste0("y = ", round(coef(lm1)[1], int.digits), " + ", round(coef(lm1)[2], 2), 
+                                    "x \nR2 = ", round(summary(lm1)$r.squared, r2.digits), 
+                                    "\nRMSE = ", rmse,
+                                    #"\nmedian percent diff = ", pct.diff.median, "%",
+                                    "\nno. pairs = ", nrow(data.wide)
+                  ),
+                  x=x.variable,
+                  y=y.variable
+             )
+           
+           
+}
+  
 
 
 #################################### temporal variables ####################################

5d61c5859fbbd5e8d9403389258eccd1922aa127
magali17 <magali@uw.edu>
2019-11-01 13:15:06 -0700

    cleaned up some fns

--- A1.0.2_Var&Fns.R
+++ A1.0.2_Var&Fns.R
@@ -6,13 +6,38 @@
 min.pct <- 0.95
 
 my.no2.units <- 10  #ppb
-my.pm25.units <- 5 #ug/m3
+my.pm25.units <- 1 #ug/m3
 
 model.digits <- 2
 
 ###################################################################################
 ################################ FUNCTIONS ######################################## 
 ###################################################################################
+####returns baseline exposure distribution 
+ expo.distrib.fn <- function(dt,
+                            years.description
+                            ) {
+   
+  df <- dt %>% 
+    group_by(pollutant) %>%
+    drop_na(exp_avg10_yr) %>%
+    dplyr::summarize(
+      years = years.description,
+      N = n(),
+      median = median(exp_avg10_yr),
+      iqr = IQR(exp_avg10_yr),
+      mean = mean(exp_avg10_yr),
+      sd = sd(exp_avg10_yr),
+      min = min(exp_avg10_yr),
+      max = max(exp_avg10_yr)
+    )
+  
+  return(df)
+  
+}
+
+###################################################################################
+
 #function renturns Table 1 summary statistics for a given dataset, with a column name describing group 
 t1.fn <- function(data=dem.bsl[dem.bsl$pollutant == "no2",], 
                   #description variables
@@ -47,9 +72,6 @@ t1.fn <- function(data=dem.bsl[dem.bsl$pollutant == "no2",],
       degree5_pct = round(degree5_n/degree_known_n*100, 1),
       degree6_n = sum(degree ==6, na.rm = T),
       degree6_pct = round(degree6_n/degree_known_n*100, 1),
-      # edu_known_n = sum(!is.na(degree)),
-      # edu_beyond_hs_n = sum(degree >2, na.rm = T),
-      # edu_beyond_hs_pct = round(edu_beyond_hs_n/N*100, 1),
       income_median = median(tr_med_inc_hshld, na.rm = T),
       income_iqr = round(IQR(tr_med_inc_hshld, na.rm = T), 1),
       income_cat_known_n = sum(!is.na(income_cat)),
@@ -61,8 +83,6 @@ t1.fn <- function(data=dem.bsl[dem.bsl$pollutant == "no2",],
       income3_pct = round(income3_n/income_cat_known_n*100, 1),
       income4_n = sum(income_cat==4, na.rm=T),
       income4_pct = round(income4_n/income_cat_known_n*100, 1),
-      #income5_n = sum(income_cat==5, na.rm=T),
-      #income5_pct = round(income5_n/income_cat_known_n*100, 1),
       apoe_known_n = sum(!is.na(apoe)),
       apoe_carrier_n = sum(apoe, na.rm=T),
       apoe_carrier_pct = round(apoe_carrier_n/apoe_known_n*100, 1),
@@ -73,14 +93,9 @@ t1.fn <- function(data=dem.bsl[dem.bsl$pollutant == "no2",],
       smoke_former_pct = round(smoke_former_n/smoke_known_n*100, 1),
       smoke_current_n = sum(smoke==2, na.rm = T),
       smoke_current_pct = round(smoke_current_n/smoke_known_n*100, 1),
-      # exercise_median = median(exercise, na.rm = T),
-      # exercise_iqr = round(IQR(exercise, na.rm = T), 1),
       exercise_regular_known_n = sum(!is.na(exercise_reg)),
       exercise_regular_n = sum(exercise_reg==1, na.rm = T),
       exercise_regular_pct = round(exercise_regular_n/exercise_regular_known_n*100, 1),
-      # bmi_median = median(bmi, na.rm = T),
-      # bmi_iqr = round(IQR(bmi, na.rm = T), 1),
-      #add categorical BMI
       bmi_known_n = sum(!is.na(bmi4)),
       bmi_under_n = sum(bmi4==0, na.rm = T),
       bmi_under_pct = round(bmi_under_n/bmi_known_n*100, 1),
@@ -117,16 +132,14 @@ t1.fn <- function(data=dem.bsl[dem.bsl$pollutant == "no2",],
       "Degree, Doctorate (n, %)" = paste0(degree5_n, " (", degree5_pct, "%)"),
       "Degree, Other (n, %)" = paste0(degree6_n, " (", degree6_pct, "%)"),
       "Census Tract Income, $ (median, IQR)" = paste0(income_median, " (", income_iqr, ")"),
-      "Census Tract Income, $ [Category 1] (n, %)" = paste0(income1_n, " (", income1_pct, "%)"),
-      "Census Tract Income, $ [Category 2] (n, %)" = paste0(income2_n, " (", income2_pct, "%)"),
-      "Census Tract Income, $ [Category 3] (n, %)" = paste0(income3_n, " (", income3_pct, "%)"),
-      "Census Tract Income, $ [Category 4] (n, %)" = paste0(income4_n, " (", income4_pct, "%)"),
-      #"Census Tract Income [Category 5] (n, %)" = paste0(income5_n, " (", income5_pct, "%)"),
+      "Census Tract Income, < $35k (n, %)" = paste0(income1_n, " (", income1_pct, "%)"),
+      "Census Tract Income, $35-50k (n, %)" = paste0(income2_n, " (", income2_pct, "%)"),
+      "Census Tract Income, $50-75k (n, %)" = paste0(income3_n, " (", income3_pct, "%)"),
+      "Census Tract Income, > $75k (n, %)" = paste0(income4_n, " (", income4_pct, "%)"),
       "APOE carrier (n, %)" = paste0(apoe_carrier_n, " (", apoe_carrier_pct, "%)"),
       "Smoke, never (n, %)" = paste0(smoke_never_n, " (", smoke_never_pct, "%)"),
       "Smoke, former (n, %)" = paste0(smoke_former_n, " (", smoke_former_pct, "%)"),
       "Smoke, current (n, %)" = paste0(smoke_current_n, " (", smoke_current_pct, "%)"),
-      #"Exercise (median, IQR)" = paste0(exercise_median, " (", exercise_iqr, ")"),
       "Regular exercise (n, %)" = paste0(exercise_regular_n, " (", exercise_regular_pct, "%)"),
       #"BMI (median, IQR)" = paste0(bmi_median, " (", bmi_iqr, ")"),
       "BMI, Underweight (n, %)" = paste0(bmi_under_n, " (", bmi_under_pct, "%)"),
@@ -155,22 +168,15 @@ t1.fn <- function(data=dem.bsl[dem.bsl$pollutant == "no2",],
   return(t1) 
   }
 
-# View(t1.fn())
-#  
-# dem.bsl %>%
-#   filter(pollutant == "no2",
-#          no2_median == "below") %>%
-#   t1.fn(data = ., column.name = "no2 below")
-
 ####################################################################################
 
 #function returns model output: HR, 95% CI, p-value
 hr.output.fn <- function(model.s = m1.s, no2.coef = "no2") {
   #model.s = m5.s
-  hr <- model.s$conf.int[no2.coef, "exp(coef)"] %>% round(model.digits) #%>% format(nsmall=model.digits) 
-  l95 <- model.s$conf.int[no2.coef, "lower .95"] %>% round(model.digits) #%>% format(nsmall=model.digits)  
-  u95 <- model.s$conf.int[no2.coef, "upper .95"] %>% round(model.digits) #%>% format(nsmall=model.digits)  
-  p <- model.s$coefficients[no2.coef, "Pr(>|z|)"] %>% round(model.digits) #%>% format(nsmall=model.digits) 
+  hr <- model.s$conf.int[no2.coef, "exp(coef)"] %>% round(model.digits)  
+  l95 <- model.s$conf.int[no2.coef, "lower .95"] %>% round(model.digits)  
+  u95 <- model.s$conf.int[no2.coef, "upper .95"] %>% round(model.digits)  
+  p <- model.s$coefficients[no2.coef, "Pr(>|z|)"] %>% round(model.digits)  
   
   #person-years used in model
   person_years <- model.s$n
@@ -179,12 +185,10 @@ hr.output.fn <- function(model.s = m1.s, no2.coef = "no2") {
   result <- data.frame(hr = hr,
                        lower_limit = l95,
                        upper_limit = u95,
-                       #ci = paste0(l95, "-", u95),
                        p = p,
                        person_years = person_years,
                        number_events = number_events)  
-  
-  
+
   return(result)
 }
 
@@ -199,7 +203,6 @@ models.fn <- function(mydata = dem.w,
                       outcome.text = "All-cause Dementia",
                       no2.var = "no2_10yr",
                       pm25.var = "pm25_10yr",
-                      #myweights = 1,
                       no2.units = my.no2.units,
                       pm25.units = my.pm25.units) {
   
@@ -208,16 +211,11 @@ models.fn <- function(mydata = dem.w,
   # surv.event = "dementia_now"
   # outcome.text = "All-cause Dementia"
   # myweights = 1
-  # no2.var = "no2_10yr"
-  # pm25.var = "pm25_10yr"
+  # no2.var = "no2_20yr"
+  # pm25.var = "pm25_20yr"
   # no2.units = my.no2.units
   # pm25.units = my.pm25.units
-  
-  
-  # #if no weights given, use 1 for all, otherwise use specific weights
-  # if (length(myweights) == 1 ) {
-  #   myweights <- rep(myweights, nrow(mydata))
-  # }
+
   
   #rename variables for fns
   mydata <- mydata %>%
@@ -266,12 +264,11 @@ models.fn <- function(mydata = dem.w,
    m1.s <- m1 %>% summary()
   
   #Model 2 (a priori): M1 + gender, education, median household income, race, birth cohort; APOE stratification.
-  #-assuming missing values (e.g., APOE) are MCAR and doing a complete case analysis. This method can be bias if values are not MCAR.
+  #assuming missing values (e.g., APOE) are MCAR and doing a complete case analysis. This method can be bias if values are not MCAR.
 
   m2 <- mydata %>%
     coxph(s.dem ~ no2 + strata(apoe) + male + race_white + income + 
             edu +  birth_cohort, 
-          #don't use edu & birth year categories b/c too few ppl w/ low degrees and early birth years, thus reference category is small/unstable? 
           data=., 
           robust = T,
           weights = model_wt
@@ -362,13 +359,13 @@ models.fn <- function(mydata = dem.w,
     )  
   
   return(list(table_of_no2_HRs= no2.hrs,
-              raw_model_output = model.ouputs
+              raw_model_output = model.ouputs,
+              survival_object = s.dem,
+              renamed_df = mydata
               )
          )
   
 }
-
-#models.fn() #[[1]]
 
 ###############################################################################################
 
@@ -376,8 +373,7 @@ models.fn <- function(mydata = dem.w,
 diff.exposures.fn <- function(mydata.2 = dem.w, 
                               surv.time2.2 = "age_end_exposure", 
                               surv.event.2 = "dementia_now", 
-                              outcome.text.2 = "All-cause Dementia" #,
-                              #weights.2 = 1
+                              outcome.text.2 = "All-cause Dementia" 
                               ) {
   
   exposure = c("1yr", "5yr", "10yr", "20yr", "10yr10yrlag", "10yr20yrlag")
@@ -410,7 +406,10 @@ diff.exposures.fn <- function(mydata.2 = dem.w,
   }
   
   myresult <- list(HRs = hr.output.df,
-                   model.output = model.output.list)
+                   model.output = model.output.list,
+                   survival.object = model_results[[3]],
+                   renamed_df = model_results[[4]]
+                   )
   
   return(myresult) 
   
@@ -441,9 +440,7 @@ hr.plot <- function(mydata,
                nrow=7) +
     theme(
       legend.position = "none",
-      #axis.text.y=element_blank(),
-      axis.ticks.y=element_blank(),
-      # strip.text.y = element_text(hjust=0,vjust = 1,angle=180
+      axis.ticks.y=element_blank()
       ) +  
   coord_flip() 
 
@@ -451,44 +448,22 @@ hr.plot <- function(mydata,
 }
 
 
-# rbind(no2_1yr_models[[1]],
-#       no2_5yr_models[[1]],
-#       no2_10yr_models[[1]],
-#       #no2_20yr_models[[1]], #error, very large CI
-#       no2_10yr10yrlag_models[[1]],
-#       no2_10yr20yrlag_models[[1]]
-#       )%>%
-#   hr.plot(.)
-
-
 ######################## replace vector NAs for IPW ########################
 #returns vector of most frequently used value if variabl is factor or mean if variable is numeric  
 
-### ?? or return most frequent value for all? 
+### ?? or return most frequent value for all so that don't get strange results for some integer values? 
 
 replace.nas.fn <- function(myvector,
                            numeric) {
-  # myvector <- apoe$education
-  # numeric <- mynumeric[1]
+ 
   
   myvector[is.na(myvector)] <- ifelse(numeric==1,
                                       mean(myvector, na.rm = TRUE),
                                       names(which.max(table(myvector)))
          )
   
-  # if (numeric == TRUE) {
-  #   myvector[is.na(myvector)] <- mean(myvector, na.rm = TRUE)
-  #   #myvector
-  #   
-  # } else {
-  #   myvector[is.na(myvector)] <- names(which.max(table(myvector)))
-  #   #myvector
-  # }
-  
   return(myvector)
 }
-
-
 
 ##############################################################################################
 #returns plot comparing HRs
@@ -534,5 +509,11 @@ comp.x.y.plot.fn <- function(data.wide,
          colour = mycolour.var
     )
   
-  
 }
+
+####################################################################################
+ 
+
+
+####################################################################################
+

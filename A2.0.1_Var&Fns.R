cdc8d0e0167c7504f0e305515153d0b314ea4332
magali17 <magali@uw.edu>
2019-12-29 14:25:53 -0800

    minor fn modifications

--- A2.0.1_Var&Fns.R
+++ A2.0.1_Var&Fns.R
@@ -66,13 +66,47 @@ few.outliers.iqr <- function(var,
   return(result)
 }
 
+################################# BC ONA Correction #################################
+#code modified from Elena Austin's. 
+#Elena: The tape position is to make sure measured values within the same attenuation threshold but different tape positions are not averaged. If you have measurements at the same position over multiple days, then I would make sure to add a term in the 'by' specification. Something like: by = c("ma200_tape_posit", "day")
 
+#takes a dataset for BC in wide format and returns ONA corrected estimates for each MA200 wavelength.
 
+makenum = function(x) {as.numeric(as.character(x))}
 
-
-
-
-
+ONA <- function(MAdata, 
+                       deltaATN=data.table(UV=0.05, 
+                                           Blue = 0.05, 
+                                           Green = 0.05, 
+                                           Red = 0.05, 
+                                           IR = 0.05)) {
+  pacman::p_load("data.table", "zoo", "plyr")
+  
+  MAdata <- MAdata %>% as.data.table()
+  
+  MAdata[ , UVthresh := round_any((`ma200_uv_atn1`)-max(`ma200_uv_atn1`)-deltaATN$UV, (deltaATN$UV+0.01), ceiling) + max(`ma200_uv_atn1`), by = c("ma200_tape_posit", "runname")]
+  MAdata[ , ona_uv := mean(makenum(`ma200_uv_bc1`)), by = c("ma200_tape_posit", "UVthresh", "runname")] 
+  
+  MAdata[ , Bluethresh := round_any((`ma200_blue_atn1`)-max(`ma200_blue_atn1`)-deltaATN$Blue, (deltaATN$Blue+0.01), ceiling) + max(`ma200_blue_atn1`), by = c("ma200_tape_posit", "runname")]    
+  MAdata[ , ona_blue := mean(makenum(`ma200_blue_bc1`)), by = c("ma200_tape_posit", "Bluethresh", "runname")] 
+  
+  MAdata[ , Greenthresh := round_any((`ma200_green_atn1`)-max(`ma200_green_atn1`)-deltaATN$Green, (deltaATN$Green+0.01), ceiling) + max(`ma200_green_atn1`), by = c("ma200_tape_posit", "runname")]    
+  MAdata[ , ona_green := mean(makenum(`ma200_green_bc1`)), by = c("ma200_tape_posit", "Greenthresh", "runname")] 
+  
+  MAdata[ , Redthresh := round_any((`ma200_red_atn1`)-max(`ma200_red_atn1`)-deltaATN$Red, (deltaATN$Red+0.01), ceiling) + max(`ma200_red_atn1`), by = c("ma200_tape_posit", "runname")]    
+  MAdata[ , ona_red := mean(makenum(`ma200_red_bc1`)), by = c("ma200_tape_posit", "Redthresh", "runname")] 
+  
+  MAdata[ , IRthresh := round_any((`ma200_ir_atn1`)-max(`ma200_ir_atn1`)-deltaATN$IR, (deltaATN$IR+0.01), ceiling) + max(`ma200_ir_atn1`), by = c("ma200_tape_posit", "runname")]    
+  MAdata[ , ona_ir := mean(makenum(`ma200_ir_bc1`)), by = c("ma200_tape_posit", "IRthresh", "runname")] 
+  
+  MAdata <- MAdata %>% as.data.frame()
+  
+  #creates issues later if you leave loaded
+  #pacman::p_unload("data.table")
+  
+  return(MAdata)
+  
+  }
 
 
 
@@ -80,7 +114,7 @@ few.outliers.iqr <- function(var,
 #returns table of distribution of a variable
 
 distribution.table <- function(dt,
-                               var.string = "ptrak_ct_cm3",
+                               var.string = "ptrak_pt_cm3",
                                round.int = 0
                                ) {
   #dt=ufp
@@ -88,78 +122,22 @@ distribution.table <- function(dt,
   # dt <- dt %>% group_by(season)
   
   t <- dt %>%
-    rename(var = var.string) %>%
+    dplyr::rename(var = var.string) %>%
     dplyr::summarize(
       N = n(),
+      #N = sum(var),
       mean_sd =  qwraps2::mean_sd (var, digits = round.int, na_rm = T, denote_sd = "paren"),
       median_iqr =  qwraps2::median_iqr(var, digits = round.int, na_rm = T, ),
       min = round(min(var), round.int),
-      max = round(max(var)), round.int)  
+      max = round(max(var)), round.int) %>%
+    select(-round.int)
   
   return(t)
 }
 
 
 
-#################################### correlation plot ####################################
-#fn takes mm dataset (in long format) and creates a scatterplot comparing readings from 2 collocated instruments 
-
-# colo.plot <- function(mydata=mm, 
-#                       primary.instrument, secondary.instrument, 
-#                       value_mean_median = "median_value",
-#                       int.digits = 0, 
-#                       r2.digits = 2, 
-#                       rmse.digits = 0) {
-#   
-#   
-#   data.wide <- mydata %>% 
-#     #select only values from desired instruments
-#     filter(instrument_id %in% c(primary.instrument, secondary.instrument)) %>%
-#     rename(value = value_mean_median) %>%
-#     select(-mean_value) %>%
-#              #make wide format
-#              spread(instrument_id, value) %>% 
-#              #only look at rows where have observations for both instruments
-#              drop_na(primary.instrument, secondary.instrument) 
-#            
-#            # data.wide <- data.wide %>%
-#            #   #calculate percent difference in estimates
-#            #   mutate(pct.diff = (data.wide[[secondary.instrument]] - data.wide[[primary.instrument]])/data.wide[[primary.instrument]]*100)
-#            
-#            
-#            lm1 <- lm(formula(paste(secondary.instrument, "~", primary.instrument)), 
-#                      data = data.wide)
-#            
-#            #percent difference between instruments
-#            #pct.diff.median <- round(median(data.wide$pct.diff, na.rm = T), 1)
-#            
-#            #rmse
-#            rmse <- (data.wide[[secondary.instrument]] - data.wide[[primary.instrument]])^2 %>%
-#              mean() %>%
-#              sqrt() %>%
-#              round(digits = rmse.digits)
-#            
-#            #compare primary & secondary instrument agreement 
-#            data.wide %>%
-#              ggplot(aes(x= data.wide[[primary.instrument]], y= data.wide[[secondary.instrument]])) + 
-#              geom_point(alpha=0.3, aes(colour = route)) + 
-#              geom_abline(intercept = 0, slope = 1) +
-#              geom_smooth(method = "lm", aes(fill="lm")) + 
-#              labs(fill="", 
-#                   title = paste0(data.wide$variable[1]),
-#                   subtitle = paste0("y = ", round(coef(lm1)[1], int.digits), " + ", round(coef(lm1)[2], 2), 
-#                                     "x \nR2 = ", round(summary(lm1)$r.squared, r2.digits), 
-#                                     "\nRMSE = ", rmse,
-#                                     "\nno. pairs = ", nrow(data.wide)
-#                   ),
-#                   x=primary.instrument,
-#                   y=secondary.instrument
-#              )
-#   
-#   
-# }
-
-#################################### correlation plot Wide format ####################################
+################################# correlation plot Wide format ####################################
 
 #fn takes dataset (in wide format) and creates a scatterplot comparing readings from 2 collocated instruments 
 colo.plot <- function(data.wide=mm.wide, 
@@ -203,11 +181,14 @@ colo.plot <- function(data.wide=mm.wide,
              geom_point(alpha=0.3, aes(col = data.wide[[col.by]]
                                        )) + 
              geom_abline(intercept = 0, slope = 1) +
-             geom_smooth(method = "lm", aes(fill="lm")) + 
+             geom_smooth(aes(fill="loess")) + 
+             geom_smooth(method = "lm", aes(fill="LS")) + 
              labs(title = mytitle,
                   x = x.label,
                   y = y.label,
-                  col = col.by) +
+                  col = col.by,
+                  fill = "fit"
+                  ) +
              annotate("text", -Inf, Inf, label = fit.info, hjust = 0, vjust = 1)
            
            return(p)
@@ -234,10 +215,10 @@ add.temporal.variables <- function(data,
   fall <- "-09-23" #usually fall starts on 22nd, sometimes on 23nd. Using 23rd for 2019 mobile monitoring campaign 
  
   mydata <- data %>% 
-    rename(
+    dplyr::rename(
       mydate = date.var
     ) %>%
-    mutate(
+    dplyr::mutate(
       hour = hour(mydate),
       #group night hours together 
       hour_night = ifelse(hour < min(early_am.), hour + 24, hour),
@@ -246,9 +227,9 @@ add.temporal.variables <- function(data,
                                        ifelse(hour %in% noon., "noon",
                                               ifelse(hour %in% evening., "evening", "night")))),
                          levels= c("early_am", "am", "noon", "evening", "night")),
-    day = wday(mydate, label = T, abbr = T),
+    day = lubridate::wday(mydate, label = T, abbr = T),
     time_of_week = factor(ifelse(wday(mydate) %in% c(1, 7), "weekend", "weekday")),
-    month = month(mydate, label = T, abbr = T),
+    month = lubridate::month(mydate, label = T, abbr = T),
     season = factor(ifelse((mydate >= ymd(paste0((year(mydate)-1), winter)) & mydate < ymd(paste0(year(mydate), spring))) |
                        mydate >= ymd(paste0(year(mydate), winter)), "winter",
                      ifelse(mydate >= ymd(paste0(year(mydate), spring)) &
@@ -331,44 +312,74 @@ ptrak.bind.fn <- function(folder_path) {
 
 #############################################################################################
 #returns boxplots of UFP estimates for selected sites by method
-ufp_by_method <- function(dt,
+ufp_by_method <- function(dt, 
+                          dt.range,
                           .months.sampled = months.sampled,
-                          add.to.title = "",
-                          methods.to.compare = c("mean_uw", "mean_s_tow2_tod2", "mean_s_tow2", "mean_s", "yhat_uw", "yhat_s", "yhat_s_tow2", "yhat_s_tow2_tod2", "yhat_m_day_hr")
+                          add.to.title = ""
                           ) {
-  #dt = annual 
+  #methods.to.compare = c("mean_uw", "mean_s_tow2_tod2", "mean_s_tow2", "mean_s", "yhat_uw", "yhat_s", "yhat_s_tow2", "yhat_s_tow2_tod2", "yhat_m_day_hr")
   
-  dt <- dt %>%
-    drop_na() %>%
-    gather(key = "method_weight", value = "ufp", methods.to.compare) %>%
-    mutate(method = ifelse(grepl(method_weight , pattern = "yhat"), "regression", "site mean")) %>%
-    separate(method_weight, into=c("method", "weight"), sep = "_" , 
-             remove = F, extra = "merge") 
+  # dt <- annual.l %>%
+  #   filter(site_no %in% s_tow2_tod2$site_no,
+  #          !weight %in% c("m_day_hr")) %>%
+  #   mutate(site_no = factor(site_no))
+  # 
+  # dt.range <-  s_tow2_tod2.rage 
   
   n <- length(unique(dt$site_no))
   
-  ufp_by_site <- dt %>%
-    #mutate(site_no = factor(site_id)) %>%
-    ggplot(aes(x=factor(site_no), y= ufp, fill=method)) + 
-    geom_boxplot(aes(), alpha=0.6) + #position="dodge"
-    geom_point(position=position_dodge(width=0.75), aes(group=method, col=weight)) +
-    #geom_line(position=position_dodge(width=0.75), aes(group=method, col=weight)) +
+  p <- dt %>%
+    ggplot(aes(x=factor(site_no), y= ufp,
+               col=method, group=method)) +  #fill=method #had fill instead of col
+    geom_pointrange(data=dt.range, 
+                    position=position_dodge(width=0.75), 
+                    aes(y=mean, ymin = min, ymax = max,
+                        #col=method
+                        ),
+                    #shape= 0, #3, #95,
+                    alpha=0.5
+                    ) + 
+    geom_point(position=position_dodge(width=0.75), aes(shape=weight)) +
     theme(axis.text.x = element_text(angle = 90)) + 
-    labs(title = paste0("Site mean UFP for ", .months.sampled[1], " - ", .months.sampled[length(.months.sampled)], " (Spring - Winter), n = ", n, " sites ", add.to.title),
+    labs(title = paste0("Site mean UFP for ", .months.sampled[1], " - ", .months.sampled[length(.months.sampled)], " (Spring - Fall), n = ", n, " sites ", add.to.title),
          y = "UFP (pt/cm3)",
-         col = "weight"
-         )
-   
-  #ufp_by_site
+         col = "Method",
+         fill = "Weight"
+    ) 
   
-  return(ufp_by_site)
+  #p
+  
+  return(p)
   
 }
+  
+    # dt <- dt %>%
+  #   drop_na() %>%
+  #   gather(key = "method_weight", value = "ufp", methods.to.compare) %>%
+  #   mutate(method = ifelse(grepl(method_weight , pattern = "yhat"), "regression", "site mean")) %>%
+  #   separate(method_weight, into=c("method", "weight"), sep = "_" , 
+  #            remove = F, extra = "merge") 
+  # 
+  # n <- length(unique(dt$site_no))
+  # 
+  # ufp_by_site <- dt %>%
+  #   #mutate(site_no = factor(site_id)) %>%
+  #   ggplot(aes(x=factor(site_no), y= ufp)) + 
+  #   #geom_boxplot(aes(), alpha=0.6) + #position="dodge"
+  #   geom_point(position=position_dodge(width=0.75), aes(fill=method, group=method, col=weight)) +
+  #   theme(axis.text.x = element_text(angle = 90)) + 
+  #   labs(title = paste0("Site mean UFP for ", .months.sampled[1], " - ", .months.sampled[length(.months.sampled)], " (Spring - Winter), n = ", n, " sites ", add.to.title),
+  #        y = "UFP (pt/cm3)",
+  #        col = "weight"
+  #        )
+  #  
+  #ufp_by_site
+  
+  #return(ufp_by_site)
+  
+#}
 
-# annual %>%
-#   select(-yhat_m_day_hr) %>%
-#   ufp_by_method()
-
+ 
  
 
 ####################################################################################################
@@ -382,9 +393,11 @@ save.pred.fn <- function(dt,
   lm1 <- lm(formula(paste0(y_string, "~", x_string)), data = dt)
   
   #save predictions (yhat)
-  dt$yhat = predict(lm1) 
+  dt$yhat = predict(lm1)
+  dt$resid_yhat = residuals(lm1)
   
   names(dt)[names(dt) == "yhat"] <- paste0("LUR_", y_string)
+  names(dt)[names(dt) == "resid_yhat"] <- paste0("resid_", y_string)
   
   return(dt)
   
